55cf79711b27e9ec1850f3338567a1f7470559a8
dibya188 <kumarcrteation75@gmail.com>
2024-06-26 00:52:32 +0530

    succesfully added"

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            The aim  behind launching online programs
+            is to evolve a seamless system of higher education. And provide
+            sustainable life-long learning and upskilling by use of innovative
+            approaches. The courses launched in the online mode will make our
+            learner highly employable on a global scale. The learning resources
+            included in online learning give a complete classroom experience to
+            our learners and help them achieve their academic and career goals.
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
